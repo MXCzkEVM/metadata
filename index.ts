33c04cd11a1d956b986a4cc27f036b190d4b2514
hairyf <wwu710632@gmail.com>
2025-04-29 12:19:49 +0800

    chore: format rpcUrls and blockExplorers in moonchain and moonchainGeneva

--- index.ts
+++ index.ts
@@ -2,14 +2,14 @@ export const moonchain = {
   id: 18686,
   name: 'Moonchain',
   nativeCurrency: { name: 'MXC', symbol: 'MXC', decimals: 18 },
-  rpcUrls: { 'default': { http: [ 'https://rpc.mxc.com' ] } },
+  rpcUrls: { 'default': { http: ['https://rpc.mxc.com'] } },
+  blockExplorers: { default: { name: 'Moonchain Explorer', url: 'https://explorer.moonchain.com' } }
 } as const;
 
 export const moonchainGeneva = {
   id: 5167004,
   name: 'Moonchain Geneva',
   nativeCurrency: { name: 'MXC', symbol: 'MXC', decimals: 18 },
-  rpcUrls: {
-    'default': { http: [ 'https://geneva-rpc.moonchain.com' ] },
-  },
+  rpcUrls: { 'default': { http: ['https://geneva-rpc.moonchain.com'] } },
+  blockExplorers: { default: { name: 'Moonchain Explorer', url: 'https://geneva-explorer.moonchain.com' } }
 } as const;
