b1383b08b56ec33e21c1832f6e45683a5a36f4ac
hairyf <wwu710632@gmail.com>
2025-09-03 12:40:07 +0800

    chore: update config

--- index.ts
+++ index.ts
@@ -3,13 +3,38 @@ export const moonchain = {
   name: 'Moonchain',
   nativeCurrency: { name: 'MXC', symbol: 'MXC', decimals: 18 },
   rpcUrls: { 'default': { http: ['https://rpc.mxc.com'] } },
-  blockExplorers: { default: { name: 'Moonchain Explorer', url: 'https://explorer.moonchain.com' } }
+  blockExplorers: { default: { name: 'Moonchain Explorer', url: 'https://explorer.moonchain.com' } },
+  icon: 'https://raw.githubusercontent.com/MXCzkEVM/metadata/main/logo-circle.svg',
 } as const;
 
 export const moonchainGeneva = {
   id: 5167004,
   name: 'Moonchain Geneva',
   nativeCurrency: { name: 'MXC', symbol: 'MXC', decimals: 18 },
-  rpcUrls: { 'default': { http: ['https://geneva-rpc.moonchain.com'] } },
-  blockExplorers: { default: { name: 'Moonchain Explorer', url: 'https://geneva-explorer.moonchain.com' } }
+  rpcUrls: { 'default': { http: ['https://geneva-rpc.mxc.com'] } },
+  blockExplorers: { default: { name: 'Moonchain Explorer', url: 'https://geneva-explorer.moonchain.com' } },
+  icon: 'https://raw.githubusercontent.com/MXCzkEVM/metadata/main/logo-circle.svg',
 } as const;
+
+export const hardhatMoonchainGeneva = {
+  name: 'Moonchain Geneva',
+  currency: { decimals: 18, name: 'MXC Token', symbol: 'MXC' },
+  explorer: { name: 'etherscan', url: 'https://geneva-explorer.moonchain.com' },
+  icon: 'https://raw.githubusercontent.com/MXCzkEVM/metadata/main/logo-circle.svg',
+  url: 'https://geneva-rpc.mxc.com',
+  chainId: 5167004,
+  type: 'http',
+  chainType: 'l1',
+  testnet: true,
+} as const;
+
+export const hardhatMoonchain = {
+  name: 'Moonchain',
+  currency: { decimals: 18, name: 'MXC Token', symbol: 'MXC' },
+  explorer: { name: 'etherscan', url: 'https://explorer.moonchain.com' },
+  icon: 'https://raw.githubusercontent.com/MXCzkEVM/metadata/main/logo-circle.svg',
+  url: 'https://rpc.mxc.com',
+  type: 'http',
+  chainId: 18686,
+  chainType: 'l1',
+}
